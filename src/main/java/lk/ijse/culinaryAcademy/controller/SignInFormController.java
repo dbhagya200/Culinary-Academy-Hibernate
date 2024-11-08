4cf1a53e41bbfa93b12680f5433e483c7d4167c9
dbhagya200 <dilinibhagya53@gmail.com>
2024-11-08 22:58:34 +0530

    created sign in and sign up interfaces

---
+++ src/main/java/lk/ijse/culinaryAcademy/controller/SignInFormController.java
@@ -0,0 +1,105 @@
+package lk.ijse.culinaryAcademy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class SignInFormController {
+
+    @FXML
+    private Label lblContactNoAlert;
+
+    @FXML
+    private Label lblEmailAlert;
+
+    @FXML
+    private Label lblFirstNameAlert;
+
+    @FXML
+    private Label lblLastNameAlert;
+
+    @FXML
+    private Label lblPasswordAlert;
+
+    @FXML
+    private Label lblSignIn;
+
+    @FXML
+    private Label lblSignUp;
+
+    @FXML
+    private Label lblUsernameAlert;
+
+    @FXML
+    private Pane signInPane;
+
+    @FXML
+    private Pane signUpPane;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void hyperForgotPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnKeyPressed(KeyEvent event) {
+
+    }
+
+}
---
+++ src/main/java/lk/ijse/culinaryAcademy/controller/SignUpFormController.java
@@ -0,0 +1,142 @@
+package lk.ijse.culinaryAcademy.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class SignUpFormController {
+
+    @FXML
+    private Label lblContactNoAlert;
+
+    @FXML
+    private Label lblEmailAlert;
+
+    @FXML
+    private Label lblFirstNameAlert;
+
+    @FXML
+    private Label lblLastNameAlert;
+
+    @FXML
+    private Label lblPasswordAlert;
+
+    @FXML
+    private Label lblSignIn;
+
+    @FXML
+    private Label lblSignUp;
+
+    @FXML
+    private Label lblUsernameAlert;
+
+    @FXML
+    private Pane signInPane;
+
+    @FXML
+    private Pane signUpPane;
+
+    @FXML
+    private TextField txtContactNo;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnMouseEntered(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnSignUpOnMouseExited(MouseEvent event) {
+
+    }
+
+    @FXML
+    void txtContactNoOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtContactNoOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtEmailOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtEmailOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtFirstNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtFirstNameOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtLastNameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtLastNameOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtPasswordOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtUsernameOnKeyPressed(KeyEvent event) {
+
+    }
+
+}
